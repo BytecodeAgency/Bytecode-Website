4408f63f46b70cb227993df3a4bbce49f519b993
Luciano Nooijen <lucianonooijen@gmail.com>
2018-08-07 21:46:24 +0200

    Added theme and static folder

---
+++ styles/theme.js
@@ -0,0 +1,31 @@
+const black = '#0f0f0f';
+const darkgray = '1a1a1a';
+const green = '#6ca57b';
+const lightgray = 'c1c1c1';
+const red  = 'df5252';
+const white = '#ffffff';
+
+const bytecode = {
+    colors: {
+        black: black,
+        darkgray: darkgray,
+        green: green,
+        lightgray: lightgray,
+        red: red,
+        white: white,
+    },
+    typography: {
+        h1: {
+
+        },
+        h2: {
+
+        },
+        h3: {
+
+        },
+    },
+};
+
+const theme = bytecode;
+export default theme;
