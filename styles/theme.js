603852f3dba7716469aa74e745454691c01a719c
Luciano Nooijen <lucianonooijen@gmail.com>
2018-08-09 16:55:55 +0200

    Convert Sketch data to theme.js object

--- styles/theme.js
+++ styles/theme.js
@@ -1,65 +1,112 @@
+/* eslint-disable no-unused-vars */
+
 // Colors
 const black = '#0f0f0f';
 const darkgray = '#1a1a1a';
 const green = '#6ca57b';
 const lightgray = '#c1c1c1';
 const mediumgray = '#656565';
-const red  = '#df5252';
+const red = '#df5252';
 const white = '#ffffff';
 
 // Fonts
+const heading = 'Lato';
+const paragraph = 'Lato';
+const menu = 'Space Mono';
 
 // Font weights
+const regular = 400;
+const bold = 700;
+const medium = bold;
+const extraBold = 900;
+
+/**
+ * Used font combinations:
+ * Lato: 900, 700, 400
+ * Space Mono: 400
+ */
 
 // Bytecode theme
 const bytecode = {
     colors: {
-        black: black,
-        darkgray: darkgray,
-        green: green,
-        lightgray: lightgray,
-        red: red,
-        white: white,
+        black,
+        darkgray,
+        green,
+        lightgray,
+        red,
+        white,
     },
     typography: {
         h1: {
             size: '4.8rem',
-            color: black,
+            height: '1.05em',
+            spacing: '0.02em',
+            font: heading,
+            weight: extraBold,
+            color: white,
         },
         h2: {
             size: '3.6rem',
-            color: black,
+            height: '1.05em',
+            spacing: '0.02em',
+            font: heading,
+            weight: medium,
+            color: white,
         },
         h3: {
             size: '2.4rem',
-            color: darkgray,
+            height: '1.2em',
+            spacing: '0.02em',
+            font: heading,
+            weight: medium,
+            color: white,
         },
         h4: {
             size: '2.0rem',
-            color: darkgray,
+            height: '1.2em',
+            spacing: '0.04em',
+            font: heading,
+            weight: medium,
+            color: white,
         },
         h5: {
             size: '1.4rem',
-            color: darkgray,
+            height: '1.4em',
+            spacing: '0.1em',
+            font: heading,
+            weight: medium,
+            color: white,
         },
         p: {
             size: '1.8rem',
-            color: mediumgray,
-        },
-        pLight: {
-            size: '1.8rem',
+            height: '1.2em',
+            spacing: '0.16em',
+            font: paragraph,
+            weight: regular,
             color: lightgray,
         },
         subtitle: {
             size: '1.6rem',
+            height: '1.2em',
+            spacing: '0.31em',
+            font: paragraph,
+            weight: bold,
             color: green,
         },
         button: {
             size: '1.4rem',
+            height: '1.2em',
+            spacing: 'normal',
+            font: paragraph,
+            weight: bold,
             color: red,
         },
         menu: {
             size: '1.6rem',
+            height: '1.2em',
+            spacing: '0.17em',
+            font: menu,
+            weight: regular,
             color: lightgray,
         },
     },
