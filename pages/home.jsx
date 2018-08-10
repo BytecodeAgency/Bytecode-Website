4173f8884ef09b07169c4f23a624091e1de3283d
Luciano Nooijen <lucianonooijen@gmail.com>
2018-08-10 14:48:47 +0200

    Added example text block to home

--- pages/home.jsx
+++ pages/home.jsx
@@ -12,21 +12,22 @@ const Home = () => (
         <div>
             <h1>Hello Home</h1>
         </div>
-        {/* <Button href="//google.com">TEKST</Button> */}
-        <TextBlock
-            subtitle="De subtitel"
-            title="De titel"
-            href="//google.com"
-            button="Call to action"
-        >
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
-            nostrum architecto cum exercitationem, numquam commodi quasi
-            voluptatem adipisci cumque sed, rerum molestias eius dicta aliquam
-            tempora facere laudantium deserunt reprehenderit quibusdam. A,
-            corrupti nemo libero molestias aliquam fugiat suscipit unde ut neque
-            consequuntur expedita beatae ex cum numquam quas vel rem est
-            assumenda nam optio in placeat!
-        </TextBlock>
+        <div style={{ margin: '10rem', maxWidth: '40rem' }}>
+            <TextBlock
+                subtitle="De subtitel"
+                title="De titel"
+                href="//google.com"
+                button="Call to action"
+            >
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
+                nostrum architecto cum exercitationem, numquam commodi quasi
+                voluptatem adipisci cumque sed, rerum molestias eius dicta
+                aliquam tempora facere laudantium deserunt reprehenderit
+                quibusdam. A, corrupti nemo libero molestias aliquam fugiat
+                suscipit unde ut neque consequuntur expedita beatae ex cum
+                numquam quas vel rem est assumenda nam optio in placeat!
+            </TextBlock>
+        </div>
     </Layout>
 );
 
