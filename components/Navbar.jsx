7e27454e2384c4a6c2510aa55dd76f517b8ef30a
Luciano Nooijen <lucianonooijen@gmail.com>
2018-08-14 21:54:33 +0200

    Navbar contents within container

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import NavbarComponents from './NavbarComponents';
+import { Container } from '../lib/Grid';
 
 const {
     NavbarContainer,
@@ -36,36 +37,38 @@ class Navbar extends React.Component {
         const { menuIsOpen } = this.state;
         return (
             <NavbarContainer>
-                <MobileNavLogo src="/static/img/logo-min.svg" />
-                <BlackOverlay
-                    menuIsOpen={menuIsOpen}
-                    onClick={this.closeMenu}
-                />
-                <MenuButton href="#" onClick={this.openMenu}>
-                    <img
-                        src="/static/img/close-menu.svg"
-                        alt="Close menu"
-                        width="100%"
+                <Container>
+                    <MobileNavLogo src="/static/img/logo-min.svg" />
+                    <BlackOverlay
+                        menuIsOpen={menuIsOpen}
+                        onClick={this.closeMenu}
                     />
-                </MenuButton>
-                <NavbarContent menuIsOpen={menuIsOpen}>
-                    <Logo src="/static/img/logo-min.svg" alt="Bytecode logo" />
-                    {/* // TODO: Link Logo to homepage */}
-                    <NavbarItems>
-                        <MenuButton href="#" onClick={this.closeMenu}>
-                            <img
-                                src="/static/img/close-menu.svg"
-                                alt="Close menu"
-                                width="100%"
-                            />
-                        </MenuButton>
-                        <NavbarItem href="/" text="Home" />
-                        <NavbarItem href="/services" text="Services" />
-                        <NavbarItem href="/over" text="Over" />
-                        <NavbarItem href="/cases" text="Cases" />
-                        <NavbarItem href="/contact" text="Contact" />
-                    </NavbarItems>
-                </NavbarContent>
+                    <MenuButton href="#" onClick={this.openMenu}>
+                        <img
+                            src="/static/img/close-menu.svg"
+                            alt="Close menu"
+                            width="100%"
+                        />
+                    </MenuButton>
+                    <NavbarContent menuIsOpen={menuIsOpen}>
+                        <Logo src="/static/img/logo-min.svg" alt="Bytecode logo" />
+                        {/* // TODO: Link Logo to homepage */}
+                        <NavbarItems>
+                            <MenuButton href="#" onClick={this.closeMenu}>
+                                <img
+                                    src="/static/img/close-menu.svg"
+                                    alt="Close menu"
+                                    width="100%"
+                                />
+                            </MenuButton>
+                            <NavbarItem href="/" text="Home" />
+                            <NavbarItem href="/services" text="Services" />
+                            <NavbarItem href="/over" text="Over" />
+                            <NavbarItem href="/cases" text="Cases" />
+                            <NavbarItem href="/contact" text="Contact" />
+                        </NavbarItems>
+                    </NavbarContent>
+                </Container>
             </NavbarContainer>
         );
     }
