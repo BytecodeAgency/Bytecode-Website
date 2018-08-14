ede59a325d6b50c4325e240f7f65dc21fb7a68df
Luciano Nooijen <lucianonooijen@gmail.com>
2018-08-14 18:47:53 +0200

    Added Drift livechat

--- pages/_document.js
+++ pages/_document.js
@@ -4,6 +4,42 @@ import Document, { Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 import '../styles/global-css';
 
+// The contents of the Drift code cannot be reused under AGPL license, please remove.
+// You can of course add your own livechat code in there, but not ours ;)
+const enableDrift = false;
+const driftCode =  `
+    "use strict";
+    !function() {
+    var t = window.driftt = window.drift = window.driftt || [];
+    if (!t.init) {
+        if (t.invoked) return void (window.console && console.error && console.error("Drift snippet included twice."));
+        t.invoked = !0, t.methods = [ "identify", "config", "track", "reset", "debug", "show", "ping", "page", "hide", "off", "on" ],
+        t.factory = function(e) {
+        return function() {
+            var n = Array.prototype.slice.call(arguments);
+            return n.unshift(e), t.push(n), t;
+        };
+        }, t.methods.forEach(function(e) {
+        t[e] = t.factory(e);
+        }), t.load = function(t) {
+        var e = 3e5, n = Math.ceil(new Date() / e) * e, o = document.createElement("script");
+        o.type = "text/javascript", o.async = !0, o.crossorigin = "anonymous", o.src = "https://js.driftt.com/include/" + n + "/" + t + ".js";
+        var i = document.getElementsByTagName("script")[0];
+        i.parentNode.insertBefore(o, i);
+        };
+    }
+    }();
+    drift.SNIPPET_VERSION = '0.3.1';
+    drift.load('bbh8h2v73h4n');
+`;
+const getDriftCode = () => {
+    if (enableDrift) {
+        return (
+            <script dangerouslySetInnerHTML={{__html: driftCode}}/>
+        );
+    }
+}
+
 export default class SiteDocument extends Document {
     render() {
         const sheet = new ServerStyleSheet();
@@ -23,6 +59,7 @@ export default class SiteDocument extends Document {
                     <link rel="apple-touch-icon" href="/static/icons/favicon.png" />
                     <link rel="manifest" href="/static/manifest.json" />
                     {styleTags}
+                    {getDriftCode()}
                 </Head>
                 <body>
                     <div className="root">{main}</div>
