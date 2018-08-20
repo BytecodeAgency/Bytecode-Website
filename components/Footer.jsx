11cfbb83977980e6c62ce72db949e7f69fb40bf8
Luciano Nooijen <lucianonooijen@gmail.com>
2018-08-20 12:17:53 +0200

    Work in progress on footer layout

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,10 +1,32 @@
+import styled from 'styled-components';
 import { Container, Row, Col } from '../lib/Grid';
 
+const FooterCol = ({ props, children }) => (
+    <Col {...props} md={6} lg={3}>
+        {children}
+    </Col>
+);
+
+const Copyright = styled.p`
+    margin: 6rem 0 4rem;
+    text-align: center;
+`;
+
 const Footer = () => (
-    <footer style={{ textAlign: 'center' }}>
-        <Container className="text">
-            &copy; {new Date().getFullYear()}
-            &nbsp;Bytecode Digital Agency B.V. - All Rights Reserved
+    <footer>
+        <Container>
+            <Row>
+                <FooterCol>Footerdinges</FooterCol>
+                <FooterCol>Footerdinges</FooterCol>
+                <FooterCol>Footerdinges</FooterCol>
+                <FooterCol>Footerdinges</FooterCol>
+            </Row>
+            <Copyright>
+                <p>
+                    &copy; {new Date().getFullYear()}
+                    &nbsp;Bytecode Digital Agency B.V. - All Rights Reserved
+                </p>
+            </Copyright>
         </Container>
     </footer>
 );
