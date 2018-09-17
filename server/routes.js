62d027204b2cf8d469420b355d6fb7845277a755
Luciano Nooijen <lucianonooijen@gmail.com>
2018-09-17 20:21:33 +0200

    Routes refactor

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,9 @@
-const routes = {};
-const staticPages = ['home', 'over', 'services', 'contact']; // eslint-disable-line
-const generateStaticPage = page => { return {[page]: { page }} }; // eslint-disable-line
-const staticPagesRoutes = staticPages.map(page => generateStaticPage(page)); // eslint-disable-line
-
-const customroutes = {};
-
-routes.push(staticPagesRoutes);
-routes.push(customroutes);
+const routes = {
+    '/': { page: '/home' },
+    '/over': { page: '/over' },
+    '/services': { page: '/services' },
+    '/contact': { page: '/contact' },
+};
 
 module.exports = routes;
+module.exports.getRoutes = () => routes;
