8c56e942490308fedba108957186719a3d796bb1
Luciano Nooijen <lucianonooijen@gmail.com>
2018-09-18 12:55:36 +0200

    Added notifications to contact form

--- components/ContactForm.jsx
+++ components/ContactForm.jsx
@@ -27,13 +27,61 @@ const SendButton = styled.button`
     padding: 2rem;
 `;
 
+const StyledNotification = styled.div`
+    color: white;
+    padding: 4rem;
+    &.success {
+        background: green;
+    }
+    &.error {
+        background: red;
+    }
+`;
+
+const Notification = ({ type, message }) => (
+    <StyledNotification className={type}>{message}</StyledNotification>
+);
+
 class ContactForm extends React.Component {
     constructor(props) {
         super(props);
+        this.getNotifications = this.getNotifications.bind(this);
+        this.addNotification = this.addNotification.bind(this);
+        this.clearNotifications = this.clearNotifications.bind(this);
         this.generateInputField = this.generateInputField.bind(this);
         this.handleInput = this.handleInput.bind(this);
         this.handleSend = this.handleSend.bind(this);
-        this.state = {};
+        this.sendData = this.sendData.bind(this);
+        this.state = {
+            notifications: [],
+            contact: '',
+            email: '',
+            phone: '',
+            content: '',
+        };
+    }
+
+    getNotifications() {
+        const { notifications } = this.state;
+        return notifications.map(notification => (
+            <Notification
+                type={notification.type}
+                message={notification.message}
+            />
+        ));
+    }
+
+    addNotification(type, message) {
+        const { notifications } = this.state;
+        notifications.push({
+            type,
+            message,
+        });
+        this.setState({ notifications });
+    }
+
+    clearNotifications() {
+        this.setState({ notifications: [] });
     }
 
     generateInputField(inputFieldType, inputFieldName, inputFieldPlaceholder) {
@@ -53,28 +101,31 @@ class ContactForm extends React.Component {
 
     handleInput(e) {
         this.setState({ [e.target.name]: e.target.value });
-        console.log(e.target.name);
-        console.log(e.target.value);
     }
 
     handleSend() {
-        const sendData = {
-            /* eslint-disable */
-            contact: this.state.contact,
-            email: this.state.email,
-            phone: this.state.phone,
-            contents: this.state.contents,
-            /* eslint-enable */
-        };
+        /* eslint-disable object-curly-newline */
+        const { contact, email, phone, contents } = this.state;
+        const sendData = { contact, email, phone, contents };
+        /* eslint-enable */
 
-        // TODO: Add field validation
+        if (!contact || !email || !phone || !contents) {
+            this.addNotification('error', 'Vul alle verplichte velden in');
+        } else {
+            this.sendData(sendData);
+        }
+    }
 
+    sendData(sendData) {
         axios
             .post('/post', sendData)
             .then(res => {
+                this.clearNotifications();
+                this.addNotification('success', 'Bericht succesvol verzonden!');
                 console.log(res); // eslint-disable-line
             })
             .catch(err => {
+                this.addNotification('error', 'Er ging iets fout...');
                 console.log(err); // eslint-disable-line
             });
     }
@@ -82,6 +133,7 @@ class ContactForm extends React.Component {
     render() {
         return (
             <ContactFormContainer>
+                {this.getNotifications()}
                 {this.generateInputField('text', 'contact', 'Naam')}
                 {this.generateInputField('email', 'email', 'Emailadres')}
                 {this.generateInputField('tel', 'phone', 'Telefoonnummer')}
