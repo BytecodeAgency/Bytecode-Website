5eeaef764e208bd6c1f16e5d986fa2f0e0e7399b
Luciano Nooijen <lucianonooijen@gmail.com>
2018-10-30 17:42:11 +0100

    Created Process component

--- pages/services.jsx
+++ pages/services.jsx
@@ -7,6 +7,7 @@ import TextBlock from '../components/TextBlock';
 import { Container, Row, Col, Hidden } from '../lib/Grid'; // eslint-disable-line
 import ContactForm from '../components/ContactForm';
 import ImageBlock from '../components/ImageBlock';
+import Process from '../components/Process';
 
 const HeaderContainer = styled.section`
     margin: 10rem 5% 5% 5%;
@@ -44,21 +45,13 @@ const HeaderImage = styled.img`
     overflow: hidden;
     width: 100%
     height: 100%;
+    z-index: -1;
 `;
 
 const ContentBlockWrapper = styled.section`
     background-color: ${theme.colors.mediumgray};
     margin: 5% 0% 5% 5%;
     padding: 7.5% 5%;
-`;
-
-const ProcessStepImage = styled.img`
-    height: 60rem;
-    max-height: 30vh;
-    margin-bottom: 5rem;
-    @media (max-width: ${theme.breakpoints[2]}px) {
-        margin-top: 10rem;
-    }
 `;
 
 const Icon = styled.img`
@@ -98,65 +91,7 @@ const Over = () => (
                 </Col>
             </Row>
         </HeaderContainer>
-        <ContentBlockWrapper>
-            <Row>
-                <Col md={4} lg={3}>
-                    <ProcessStepImage
-                        src="/static/img/content/workflow/analyze.svg"
-                        alt="Analyze"
-                    />
-                    <TextBlock
-                        subtitle="Stap 1"
-                        headingType="h2"
-                        title="Analyze"
-                    >
-                        Voordat we starten met ontwikkelen, maken we eerst een
-                        gedetailleerde analyse. Dit stelt ons in staat om een
-                        helder en volledig overzicht te krijgen van de huidige
-                        situatie. Hierdoor kunnen wij de pijnpunten in beeld
-                        brengen en analyseren welke punten het meeste prioriteit
-                        hebben.
-                    </TextBlock>
-                </Col>
-                <Col offset={{ lg: 1 }} md={4} lg={3}>
-                    <ProcessStepImage
-                        src="/static/img/content/workflow/strategize.svg"
-                        alt="Strategize"
-                    />
-                    <TextBlock
-                        subtitle="Stap 2"
-                        headingType="h2"
-                        title="Strategize"
-                    >
-                        Vervolgens maken wij een duidelijke strategie, waarin we
-                        uiteenzetten welke punten we aan gaan pakken en vooral
-                        op welke manier we dat doen. Bij het ontwikkelen van
-                        deze strategie wegen we de verschillende opties af,
-                        zodat we het best haalbare resultaat met het oog op de
-                        toekomst gaan halen.
-                    </TextBlock>
-                </Col>
-                <Col offset={{ lg: 1 }} md={4} lg={3}>
-                    <ProcessStepImage
-                        src="/static/img/content/workflow/realize.svg"
-                        alt="Realize"
-                    />
-                    <TextBlock
-                        subtitle="Stap 3"
-                        headingType="h2"
-                        title="Realize"
-                    >
-                        Nadat we alles helemaal hebben geanalyseerd en gepland,
-                        is het tijd om de handen uit de mouwen te steken en het
-                        plan te realiseren. Hierbij werken we volgens de
-                        scrum-methode. Op het moment dat het plan uitgevoerd is,
-                        is het tijd om het resultaat te analyzeren en om te
-                        kijken hoe het nóg beter kan. We blijven dus verbeteren
-                        en doorontwikkelen.
-                    </TextBlock>
-                </Col>
-            </Row>
-        </ContentBlockWrapper>
+        <Process />
         <StyledContainer>
             <Row>
                 <Col lg={5} md={6}>
@@ -339,7 +274,7 @@ const Over = () => (
                     </Row>
                 </Col>
 
-                <Col fluid offset={{ xl: 1 }} lg={6}>
+                <Col fluid="true" offset={{ xl: 1 }} lg={6}>
                     <ImageBlock
                         src="/static/img/content/group-full.jpg"
                         alt="groep"
