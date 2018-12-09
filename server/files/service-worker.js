c706804b4b78f697eecf58ee7f078f8898bab158
Luciano Nooijen <luciano@bytecode.nl>
2018-12-09 13:55:54 +0100

    Fixed linting errors

--- server/files/service-worker.js
+++ server/files/service-worker.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-restricted-globals, no-undef, no-console */
 
 console.log('Service worker file opened!');
-/* Service worker conflicts with serving pdf's. When that's fixed we'll reregister service workers.
+/* Service worker conflicts with serving pdf's.
+   When that's fixed we'll reregister service workers.
 const CACHE_NAME = 'bytecode';
 const urlsToCache = ['/', '/over', '/services', '/cases', '/contact'];
 
