6c09cb29a0c64d362e5510c208167a250fd8de19
Luciano Nooijen <luciano@bytecode.nl>
2019-03-06 09:44:29 +0100

    Fixed ESLint errors

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -42,8 +42,8 @@ class Navbar extends React.Component {
                 <InnerNavbarContainer>
                     <Link prefetch href="/">
                         <a>
-                            <MobileNavLogo 
-                                src="/static/img/logo-min.svg" 
+                            <MobileNavLogo
+                                src="/static/img/logo-min.svg"
                                 alt="Logo"
                             />
                         </a>
