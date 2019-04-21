a49c9c55cd75550f376e9d4d880342f28142d08c
Luciano Nooijen <luciano@bytecode.nl>
2019-04-21 13:42:10 +0200

    Added 404 page
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

---
+++ src/pages/404.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import Layout from '../components/Layout';
+
+const Container404 = styled.div`
+    text-align: center;
+    margin: 20rem 0;
+`;
+
+const pageSettings = {
+    title: 'Error',
+    description: 'Er ging iets fout',
+    keywords: 'error',
+};
+
+const NotFound = () => (
+    <Layout pageSettings={pageSettings}>
+        <Container404>
+            <h1>Oeps, deze pagina bestaat niet...</h1>
+        </Container404>
+    </Layout>
+
+);
+
+export default NotFound;
