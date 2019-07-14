80c5d24f0a85af6cd3b2c167a5984fdb83ef0163
Richard van 't Hof <richardvanthof@pm.me>
2019-07-14 19:55:16 +0200

    Added more elements for cases page
    Experimented with lay-outs with images, text and the forms behind them.
    Something that is makes this process difficult is the lack of
    a contained container that is wider than the one we currently have.

--- src/pages/case.jsx
+++ src/pages/case.jsx
@@ -26,6 +26,52 @@ const Statistic = styled(Col)`
     padding-bottom: 1em;
 `;
 
+const Checklist = styled.ul`
+    margin: 2em 0;
+`;
+
+const CheckListItemBase = styled.li`
+    display: flex;
+    align-items: center;
+    justify-items: left;
+    margin: 1.33em 0;
+    p {
+        margin: 0;
+    }
+    img {
+        width: 2rem;
+        margin-right: 1.33em;
+    }
+`;
+
+const PaddedContainer = styled(Container)`
+    padding: 7em 0;
+`;
+
+const CheckListItem = props => {
+    const { children } = props;
+    return (
+        <CheckListItemBase>
+            <img src={require('../images/icons/ui/check.svg')} alt="- " />
+            <p>
+                <big>{children}</big>
+            </p>
+        </CheckListItemBase>
+    );
+};
+
+const Frame = styled.figure`
+    background: teal;
+    overflow: hidden;
+
+    padding: 0em;
+    transform: translateX(10em);
+    img {
+        height: 100%;
+        transform: translate(5rem, 5rem) scale(1.1);
+    }
+`;
+
 const Results = styled(Container)`
     padding: 5em 0;
     &:before {
@@ -44,8 +90,165 @@ const Results = styled(Container)`
 
 const testImg = require('../images/img/content/team.png');
 
+const WrappedContainer = styled(PaddedContainer)`
+    &:before {
+        content: '';
+        position: absolute;
+        top: -3em;
+        right: 0em;
+        width: 96vw;
+        height: 175%;
+        background: ${theme.colors.secondary};
+    }
+`;
+
 const Case = () => (
     <Layout pageSettings={pageSettings}>
+        <Container fluid>
+            <Row>
+                <Col offset={{ lg: 1 }} lg={5}>
+                    <h1>Cote et Ciel</h1>
+                    <p>
+                        <big>
+                            We helped the educational organisation sales
+                            leadership academy get back it leadership in sales
+                            by great design and converstion. The goal was to
+                            make the aging site new again.d
+                        </big>
+                    </p>
+                    <h3>Challange</h3>
+                    <p>
+                        (Hier wordt de opdracht en dus het doel beschreven, bij
+                        meerdere (sub) doelen mogen bulletpoints gebruikt
+                        worden) .We helped the educational organisation sales
+                        leadership academy get back it leadership in sales by
+                        great design and converstion. The goal was to make the
+                        aging site new again.
+                    </p>
+                    <Checklist>
+                        <CheckListItem>
+                            Altijd duidelijke en snelle communicatie
+                        </CheckListItem>
+                        <CheckListItem>
+                            7 dagen per week, binnen een uur, reactie op vragen
+                        </CheckListItem>
+                        <CheckListItem>
+                            Voor noodgevallen altijd bereikbaar te zijn
+                        </CheckListItem>
+                        <CheckListItem>
+                            Voor noodgevallen altijd bereikbaar te zijn
+                        </CheckListItem>
+                        <CheckListItem>
+                            Transparantie en openheid, zonder uitzonderingen
+                        </CheckListItem>
+                    </Checklist>
+                </Col>
+                <Col lg={6}>
+                    <img
+                        src={require('../images/img/content/cases/cote-et-ciel/website1.png')}
+                        alt="Website Cote et Ciel"
+                    />
+                </Col>
+            </Row>
+        </Container>
+        <WrappedContainer fluid>
+            <Row align="center">
+                <Col offset={{ lg: 1 }} lg={4}>
+                    <TextBlock
+                        headingType="h2"
+                        title="Leg de focus op de webshop"
+                        subtitle="discover"
+                    >
+                        [hier leggen we uit hoe we doe doelen wilde bbereieken
+                        of war we achter zijn gekomen in research] We kwamen
+                        erachter dat de site te veel aanvoelde als een gallerij
+                        en gingen daarom de sitae inrichten rond de webshop
+                    </TextBlock>
+                </Col>
+                <Col offset={{ lg: 1 }} lg={6}>
+                    <Frame>
+                        <img
+                            src={require('../images/img/content/cases/cote-et-ciel/website2.png')}
+                            alt="Website Cote et Ciel"
+                        />
+                    </Frame>
+                </Col>
+            </Row>
+        </WrappedContainer>
+        <Container fluid>
+            <Row align="center">
+                <Col lg={6}>
+                    <Frame left>
+                        <img
+                            src={require('../images/img/content/cases/cote-et-ciel/admin-panel.jpg')}
+                            alt="Website Cote et Ciel"
+                        />
+                    </Frame>
+                </Col>
+                <Col offset={{ lg: 1 }} lg={4}>
+                    <TextBlock
+                        headingType="h2"
+                        title="Toewerken naar een schaalbare infrastructuur"
+                    >
+                        [hier leggen we uit hoe we doe doelen wilde bbereieken
+                        of war we achter zijn gekomen in research] We kwamen
+                        erachter dat de site te veel aanvoelde als een gallerij
+                        en gingen daarom de sitae inrichten rond de webshop
+                    </TextBlock>
+                </Col>
+            </Row>
+        </Container>
+        <PaddedContainer fluid>
+            <Row align="center">
+                <Col offset={{ lg: 1 }} lg={4}>
+                    <TextBlock
+                        headingType="h2"
+                        subtitle="Design & Development"
+                        title="Seamless One-Step Checkout"
+                    >
+                        [hier leggen we uit hoe we doe doelen wilde bbereieken
+                        of war we achter zijn gekomen in research] We helped the
+                        educational organisation sales leadership academy get
+                        back it leadership in sales by great design and
+                        converstion. The goal was to make the aging site new
+                        again.
+                    </TextBlock>
+                </Col>
+                <Col offset={{ lg: 1 }} lg={6}>
+                    <Frame left>
+                        <img
+                            src={require('../images/img/content/cases/cote-et-ciel/checkout.png')}
+                            alt="Website Cote et Ciel"
+                        />
+                    </Frame>
+                </Col>
+            </Row>
+        </PaddedContainer>
+        <PaddedContainer fluid>
+            <Row align="center">
+                <Col offset={{ lg: 1 }} lg={4}>
+                    <TextBlock
+                        headingType="h2"
+                        title="Een Infrastructuur Waarop Je Kan Bouwen"
+                    >
+                        [hier leggen we uit hoe we doe doelen wilde bbereieken
+                        of war we achter zijn gekomen in research] We helped the
+                        educational organisation sales leadership academy get
+                        back it leadership in sales by great design and
+                        converstion. The goal was to make the aging site new
+                        again.
+                    </TextBlock>
+                </Col>
+                <Col offset={{ lg: 1 }} lg={5}>
+                    <Frame left>
+                        <img
+                            src={require('../images/img/content/cases/cote-et-ciel/server.jpg')}
+                            alt="Website Cote et Ciel"
+                        />
+                    </Frame>
+                </Col>
+            </Row>
+        </PaddedContainer>
         <Wrapper>
             <Container>
                 <Row>
@@ -114,19 +317,19 @@ const Case = () => (
                         gutterWidth={200}
                         align="center"
                     >
-                        <Statistic md={6}>
+                        <Statistic xl={4} md={6}>
                             <h1>
                                 100<sup>%</sup>
                             </h1>
                             <p>Uptime</p>
                         </Statistic>
-                        <Statistic md={6}>
+                        <Statistic xl={4} md={6}>
                             <h1>
                                 5<sup>x</sup>
                             </h1>
                             <p>Meer conversie</p>
                         </Statistic>
-                        <Statistic md={6}>
+                        <Statistic xl={4} md={6}>
                             <h1>
                                 3<sup>x</sup>
                             </h1>
