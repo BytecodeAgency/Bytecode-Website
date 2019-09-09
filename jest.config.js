efddb4c6a7d6b2ef4b4799adfbb7686d42d47705
Luciano Nooijen <luciano@bytecode.nl>
2019-09-09 22:16:38 +0200

    Added Jest to CI
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
     clearMocks: true,
     coverageDirectory: 'coverage',
+    collectCoverageFrom: ['src/**/*.(js)'], // TODO: Make Jest work with JSX
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     // setupFiles: ['./tests/config/global-setup.ts'],
-    // testEnvironment: 'node',
-    testRegex: 'src/(.*).(test).js?',
+    testEnvironment: 'node',
+    testRegex: 'src/(.*).(test).(js|jsx)?',
 };
