0dd79aed354365026fcacd8e27b67223a9d9f417
Luciano Nooijen <luciano@bytecode.nl>
2019-10-07 19:46:55 +0200

    Revert "Prepare release"
    This reverts commit 2b0b25018d2875a40c28699408db290c2db2359f.

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -65,6 +65,7 @@ class Navbar extends React.Component {
                         </CloseMenuButton>
                         <NavbarItem href="/" text="Home" />
                         <NavbarItem href="/services" text="Services" />
+                        <NavbarItem href="/cases" text="Work" />
                         <NavbarItem href="/insights" text="Insights" />
                         <NavbarItem href="/over" text="Over" />
                         <NavbarItem href="/contact" text="Contact" />
