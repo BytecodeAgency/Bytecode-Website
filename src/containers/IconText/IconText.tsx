eeceace23381ee5c38f3f573c7deb41f9bbe9dd9
Luciano Nooijen <luciano@bytecode.nl>
2021-06-08 19:34:22 +0200

    Fix broken linting
    Signed-off-by: Luciano Nooijen <luciano@bytecode.nl>

--- src/containers/IconText/IconText.tsx
+++ src/containers/IconText/IconText.tsx
@@ -1,30 +1,27 @@
 import React from 'react';
+import { FaRegClock } from '@react-icons/all-files/fa/FaRegClock';
+import { FaMapMarkerAlt } from '@react-icons/all-files/fa/FaMapMarkerAlt';
 import { IconTextProps, IconProps } from './IconText.types';
 import { IconTextContainer, IconStyle } from './IconText.components';
-import { FaRegClock, FaMapMarkerAlt } from "react-icons/fa";
 
 // Creates the Icon
 const Icon: React.FC<IconProps> = ({ iconName, iconColor }) => {
     let icon;
-    if (iconName === "FaMapMarkerAlt") {
-        icon = <FaMapMarkerAlt />
+    if (iconName === 'FaMapMarkerAlt') {
+        icon = <FaMapMarkerAlt />;
     }
-    if (iconName === "FaRegClock") {
-        icon = <FaRegClock />
+    if (iconName === 'FaRegClock') {
+        icon = <FaRegClock />;
     }
     return <IconStyle style={{ color: iconColor }}>{icon}</IconStyle>;
-}
+};
 
 // Creates the Icon with Text
-const IconText: React.FC<IconTextProps> = ({
-    iconName,
-    iconColor,
-    text
-}) => {
+const IconText: React.FC<IconTextProps> = ({ iconName, iconColor, text }) => {
     return (
         <IconTextContainer>
             <Icon iconName={iconName} iconColor={iconColor} />
-            <span style={{ fontSize: "15px" }}>{text}</span>
+            <span style={{ fontSize: '15px' }}>{text}</span>
         </IconTextContainer>
     );
 };
