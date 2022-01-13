60c81e30f5d7f1208119bb6b4040e0009a952c35
Jeroen van Steijn <jeroen@bytecode.nl>
2022-01-13 11:54:39 +0100

    Remove components folder from components library

--- components/src/index.ts
+++ components/src/index.ts
@@ -1,7 +1,7 @@
 
 export { default as darkTheme } from './themes/dark'
 export { default as defaultTheme } from './themes/default'
-export { default as GlobalStyles } from './components/GlobalStyles'
+export { default as GlobalStyles } from './GlobalStyles'
 
-export { default as Button } from './components/Button'
-export { default as Input } from './components/Input'
+export { default as Button } from './Button'
+export { default as Input } from './Input'
