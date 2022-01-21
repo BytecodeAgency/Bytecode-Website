05e364a189bc75e41c4b6802f986f770ba139832
Jeroen van Steijn <jeroen@bytecode.nl>
2022-01-21 15:14:56 +0100

    Implement mobile menu with buttons and animations

--- components/src/components/Menu/MobileMenu.tsx
+++ components/src/components/Menu/MobileMenu.tsx
@@ -2,51 +2,98 @@ import React from "react"
 import Link from "next/link"
 import { useState } from "react"
 import { MenuInterface } from "./Menu.types"
-import { CrossSVG } from "src/assets/CrossSVG"
+import { CrossSVG } from "../../assets/CrossSVG"
+import { HamburgerSVG } from "../../assets/HamburgerSVG"
+import styled from "styled-components"
 
-export const MobileMenu: React.FC<MenuInterface> = ({navLinks, children}) => {
-    const [isOpen] = useState(true);
+const MobileMenuBarTopContainer = styled.div<{ hide?: boolean }>`
+    display: grid;
+    grid-template-columns: 1fr 50px;
+    padding-top: 25px;
+    visibility: ${props => props.hide ? "hidden" : "unset"};
+`
 
-    if(isOpen) {
+const MobileMenuContainer = styled.div<{ isOpen: boolean }>`
+    display: ${props => props.isOpen ? "block" : "none"};
+    height: 100vh;
+    width: 100vw;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: white;
+    opacity: ${props => props.isOpen ? 1 : 0};
+    animation: fadeIn 0.3s ease-in;
+    @keyframes fadeIn {
+        0% {
+            opacity: 0;
+            transform: translateY(-60px);
+        }
+        100% {
+            opacity: 1;
+            transform: translateY(0);
+        }
+    }
+`
+
+export const MobileMenu: React.FC<MenuInterface> = ({ navLinks, children }) => {
+    const [isOpen, setOpen] = useState(false);
+
+    const open = () => {
+        setOpen(true)
+    }
+
+    const close = () => {
+        setOpen(false)
+    }
+
+    const Unopened = () => {
         return (
+    
+            <MobileMenuBarTopContainer hide={isOpen}>
             <div>
-                <div>
-                    <div>
-                        {children}
-                    </div>
-                    <div>
-                        <CrossSVG />
-                    </div>
-                </div>
-                <ul>
-                    {navLinks.map((item) => (
-                        <li key={item.name}>
-                            <Link key={item.name} href={item.href}>{item.name}</Link>
-                        </li>
-                    ))}
-                </ul>
-                <div>
-                    <div>
-                        Send us an email
-                    </div>
-                    <div>
-                        Plan a meeting
-                    </div>
-                </div>
+                {children}
             </div>
+            <div onClick={open} >
+                <HamburgerSVG />
+            </div>
+        </MobileMenuBarTopContainer>
+        )
+    }
+    
+    const Opened = () => {
+        return (
+                <MobileMenuContainer isOpen={isOpen}>
+                    <MobileMenuBarTopContainer>
+                        <div>
+                            {children}
+                        </div>
+                        <div onClick={close}>
+                            <CrossSVG />
+                        </div>
+                    </MobileMenuBarTopContainer>
+                    <ul>
+                        {navLinks.map((item) => (
+                            <li key={item.name}>
+                                <Link key={item.name} href={item.href}>{item.name}</Link>
+                            </li>
+                        ))}
+                    </ul>
+                    <div>
+                        <div>
+                            Send us an email
+                        </div>
+                        <div>
+                            Plan a meeting
+                        </div>
+                    </div>
+                </MobileMenuContainer>
         )
     }
 
     return (
-        <div>
-            <div>
-                {children}
-            </div>
-            <div>
-                -<br />
-                -<br />
-                -<br />
-            </div>
-        </div>
+        <>
+            <Unopened />
+            <Opened />
+        </>
     )
 }
--- components/webpack.config.js
+++ components/webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  devtool: 'inline-source-map',
   entry: './src/index.ts',
   module: {
     rules: [
