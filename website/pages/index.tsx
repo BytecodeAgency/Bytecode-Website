0e3b99c63f56dfcd72b22fbf6e81d6b62ac0e75e
Tiko <tiko@bytecode.nl>
2022-05-09 17:19:25 +0200

    added pictures and who are we section

--- website/pages/index.tsx
+++ website/pages/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import type { NextPage } from "next";
-import { Container, InitialContainer, Spacer, Heading, Paragraph } from "@bytecode/ui-library/components";
+import { Container, InitialContainer, Spacer, Heading, Paragraph, ArrowLink } from "@bytecode/ui-library/components";
 import MainLayout from "layout/MainLayout";
 import styled from "styled-components";
-import { breakpointNameToPx, responsiveValuesCSS } from "@bytecode/ui-library/utils";
+import { breakpointNameToPx, responsiveValuesCSS, Employees, isWindowSizeBiggerThan } from "@bytecode/ui-library/utils";
+import Image from "next/image";
 
 const content = {
 	title: "Bytecode",
@@ -47,6 +48,8 @@ const coFounderTextContainerResponsiveCSS = responsiveValuesCSS(
 const CoFounderTextContainer = styled.div`
 	${coFounderTextContainerResponsiveCSS};
 	display: grid;
+	grid-column-gap: 40px;
+	padding-left: 20px
 `;
 
 const CoFounderHeading = styled(Heading)`
@@ -66,13 +69,81 @@ const BytecodeAsCoFounder = () => (
 				but aren’t always interested in your idea or the success."
 			/>
 		</CoFounderTextContainer>
-
 	</Container>
 );
+
+const whoWeAreContainerResponsiveCSS = () => {
+	const gridColumns = responsiveValuesCSS(
+		"grid-template-columns",
+		"",
+		breakpointNameToPx({
+			xs: "1fr",
+			lg: "1fr 1fr"
+		})
+	);
+	const paddingTop = responsiveValuesCSS(
+		"padding-top",
+		"px",
+		breakpointNameToPx({
+			xs: 40,
+			lg: 100
+		})
+	);
+	return gridColumns + paddingTop;
+};
+
+const WhoWeAreContainer = styled.div`
+	${whoWeAreContainerResponsiveCSS};
+	display: grid;
+	grid-column-gap: 40px;
+`;
+
+const WhoWeAreTextContainer = styled.div`
+	align-self: center;
+	display: grid;
+`;
+
+const MeetTheTeamButton = styled(ArrowLink)`
+	justify-self: end;
+`;
+
+const PicturesContainer = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	gap: 20px;
+	justify-content: center;
+	padding-bottom: 40px;
+`;
+
 const WhoWeAre = () => (
-	<div>
-		who are we?
-	</div>
+	<WhoWeAreContainer>
+		<PicturesContainer>
+			{
+				Employees.map((employee, index) => (
+					isWindowSizeBiggerThan("xxl")
+						? <Image key={index} src={`/images/member-${employee}.png`} alt="profile picture" width={180} height={270} />
+						: <Image key={index} src={`/images/member-${employee}.png`} alt="profile picture" width={150} height={225} />
+				))
+			}
+		</PicturesContainer>
+		<WhoWeAreTextContainer>
+			<Heading type="h2" text="Who are we?" />
+			<Paragraph text="We want to be your tech co-founder & CTO.
+				Together with you, we build your start-up and digital product.
+				Our core expertise is designing and programming your MVP.
+				But before we develop anything, we work together with you to understand the ideal user experience and the user's goals.
+				That enables us to create a product scope. With this input,
+				we develop functionalities and work towards a first working version of your concept."
+			/>
+			<Paragraph text="To enhance our collaboration we often invest in the start-ups that we work with.
+				This makes it more affordable to create an MVP and ensures you have a tech team for the long term."
+			/>
+			<MeetTheTeamButton
+				text="Meet the team"
+				link="/who-we-are"
+			/>
+		</WhoWeAreTextContainer>
+	</WhoWeAreContainer>
 );
 
 const WhoIsBytecode = () => (
