c3269ba5a2630afc0088a7465356d8bc7c0a8a01
Tiko <tiko@bytecode.nl>
2022-05-10 15:28:27 +0200

    added Who is bytecode section to homepage folder

--- components/src/sections/HomePage/index.ts
+++ components/src/sections/HomePage/index.ts
@@ -3,3 +3,6 @@ export * from "./MeetTheFounders";
 
 export { default as Intro } from "./Intro";
 export * from "./Intro";
+
+export { default as WhoIsBytecode } from "./WhoIsBytecode";
+export * from "./WhoIsBytecode";
