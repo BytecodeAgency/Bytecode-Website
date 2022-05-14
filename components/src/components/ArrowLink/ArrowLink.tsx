182f0d216bc7ea8220049ec083f0ce2e3ad64b38
Tiko <tiko@bytecode.nl>
2022-05-14 15:19:20 +0200

    fixed error for that was created by importing a function from content to ArrowLink

--- components/src/components/ArrowLink/ArrowLink.tsx
+++ components/src/components/ArrowLink/ArrowLink.tsx
@@ -4,7 +4,6 @@ import { Paragraph } from "../Typography";
 import { DiagonalArrow } from "../../icons";
 import { WithStyle } from "../../types/utils";
 import Link from "next/link";
-import { getRandomCase } from "../../content";
 
 type ArrowLinkProps = {
 	onClick?: () => void;
@@ -36,10 +35,9 @@ const ArrowLink = ({ onClick, text, className, link }: WithStyle<ArrowLinkProps>
 	const [isHover, setIsHover] = useState(false);
 
 	const Arrow = () => <ArrowContainer rotation={isHover}><DiagonalArrow color="black" size={24} /></ArrowContainer>;
-	const randomCaseName = getRandomCase();
 	return (
 		link ? (
-			<Link href={link == "randomCase" ? `/case/${randomCaseName}` : link }>
+			<Link href={link}>
 				<ArrowLinkContainer className={className}
 					onMouseEnter={() => setIsHover(true)}
 					onMouseLeave={() => setIsHover(false)}>
