37c9842ae6fb96d5f3f5326e60671e55dc27866a
Jeroen van Steijn <jeroen@bytecode.nl>
2022-05-17 15:06:18 +0200

    Build bytecode component library from github

--- website/next.config.js
+++ website/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withTM = require("next-transpile-modules")(["../components"]);
+const withTM = require("next-transpile-modules")(["../components", "@bytecode/ui-library"]);
 module.exports = withTM({
 	reactStrictMode: true,
 });
